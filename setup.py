e718ad8da1e118d14167b525a8fbc20aabf7131e
kelu124 <kelu124@gmail.com>
2020-11-04 16:47:08 +0100

    v0.2.1 - solving imports

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'un0usb',         # How you named your package folder (MyLib)
   packages = ['un0usb'],   # Chose the same as "name"
-  version = '0.2.0',      # Start with a small number and increase it with every change you make
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library for un0rick usb interface',   # Give a short description about your library
   author = 'Luc Jonveaux',                   # Type in your name
   author_email = 'kelu124@gmail.com',      # Type in your E-Mail
   url = 'http://un0rick.cc',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kelu124/python-usb-un0rick/archive/v0.2.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kelu124/python-usb-un0rick/archive/v0.2.1.tar.gz',    # I explain this later on
   keywords = ['ultrasound', 'usb', 'un0rick'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyftdi','numpy','matplotlib',
---
+++ un0usb/__init__.py
@@ -0,0 +1,3 @@
+from csr_map import CsrMap
+from fpga_ctrl import FpgaControl
+from ftdi_dev import FtdiDevice
